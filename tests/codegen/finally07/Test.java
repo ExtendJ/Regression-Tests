4896715d54de9325f75216eece57354e422aaaf9
Jesper Öqvist <jesper.oqvist@cs.lth.se>
2013-08-14 16:22:55 +0200

    Added codegen tests

---
+++ tests/codegen/finally07/Test.java
@@ -0,0 +1,29 @@
+public class Test {
+
+	public static void main(String argv[]) {
+		int x = 0;
+		int y = 0;
+		int i;
+		for ( i = 0; i < 2; i++ ) {
+			try {
+				try {
+					if ( i == 0 ) {
+						continue;
+					}
+				}
+				finally {
+					x += 1;
+				}
+			} finally {
+				y += x*3;
+			}
+		}
+
+		if (y != 9) {
+			System.out.println("FAIL");
+			System.out.println("expected y = 9 but was " + y);
+			System.exit(1);
+		}
+	}
+}
+

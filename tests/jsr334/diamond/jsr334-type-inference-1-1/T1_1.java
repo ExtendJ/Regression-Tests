fbd71c8e7b02a0404084ab43d1ad5efb46f088c7
Jesper Öqvist <jesper.oqvist@cs.lth.se>
2013-08-24 09:01:29 +0200

    Cleaned up old jacks tests

--- tests/jsr334/diamond/jsr334-type-inference-1-1/T1_1.java
+++ tests/jsr334/diamond/jsr334-type-inference-1-1/T1_1.java
@@ -1,9 +1,6 @@
-
 public class T1_1 {
     T1_1 (){}
     public static void main(String[] args) {
-        
 		java.util.ArrayList<Integer> foo = new java.util.ArrayList<>();
-	
     }
 }
--- tests/jsr334/diamond/jsr334-type-inference-10/T10.java
+++ tests/jsr334/diamond/jsr334-type-inference-10/T10.java
@@ -1,9 +1,6 @@
-
 public class T10 {
     T10 (){}
     public static void main(String[] args) {
-        
 		java.util.List<Integer>[] a = new java.util.ArrayList<>[12];
-	
     }
 }

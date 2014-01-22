7a736d13e79a726ed56c003be6ea26a8ae29442f
Erik Hogeman <ada09eho@student.lu.se>
2014-01-22 13:41:09 +0100

    Added lambda parse tests

--- tests/lambda/name02/Test.java
+++ tests/lambda/name02/Test.java
@@ -3,6 +3,6 @@ import java.util.concurrent.Callable;
 public class Test {
 	public static void main(String[] args) {
         Object x = 0;
-		Object f = (int x) -> { return x.toString(); };
+		Object f = (String x) -> { return x.toString(); };
     }
 }

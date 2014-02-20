b1c8a488ba9bf717f808dc3187b037b1f1c34c7f
Jesper Öqvist <jesper.oqvist@cs.lth.se>
2014-02-20 15:13:58 +0100

    Added multiplicity tests

---
+++ tests/mult/output02/Test.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Test {
+	static class Account {
+		String name;
+		public Account(String name) {
+			this.name = name;
+		}
+		@Override
+		public String toString() {
+			return name;
+		}
+	}
+
+	static @any(Set) Account accounts = [[new HashSet<Account>() { { add(new Account("Steve")); } }]];
+
+	public static void main(String[] args) {
+		int numAccount = [[accounts]].size();
+		System.out.println("Num accounts: " + numAccount);
+	}
+}
+

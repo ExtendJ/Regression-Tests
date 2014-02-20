310e439903318333c3ea546747cde3d6652dec8d
Erik Hogeman <ada09eho@student.lu.se>
2014-02-20 16:29:41 +0100

    Changed java 8 parse tests to expect parentheses pretty print

--- tests/jsr335/Parsing/andbitwise/andbitwise3/Test.java
+++ tests/jsr335/Parsing/andbitwise/andbitwise3/Test.java
@@ -1,7 +1,6 @@
-
 public class Test {
-	public static void main(String[] args) {
-		while(a & b) 
-			methodCall();
-    }
+  public static void main(String[] args) {
+    while((a & b))
+      methodCall();
+  }
 }
--- tests/jsr335/Parsing/andbitwise/andbitwise5/Test.java
+++ tests/jsr335/Parsing/andbitwise/andbitwise5/Test.java
@@ -1,8 +1,6 @@
-
 public class Test {
-	public static void main(String[] args) {
-		if((a & b)(c & d))
-			var = (e & f)methodCall(g & h);
-			
-    }
+  public static void main(String[] args) {
+    if(((a & b)((c & d)))) 
+      (var = ((e & f)methodCall((g & h))));
+  }
 }

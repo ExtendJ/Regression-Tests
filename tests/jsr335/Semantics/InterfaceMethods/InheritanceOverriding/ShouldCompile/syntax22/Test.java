7c16465af1c3f5010252b6d21d45ae61c5df7e58
Erik Hogeman <ada09eho@student.lu.se>
2014-03-26 13:15:21 +0100

    Added more interface method tests

---
+++ tests/jsr335/Semantics/InterfaceMethods/InheritanceOverriding/ShouldCompile/syntax22/Test.java
@@ -0,0 +1,16 @@
+public class Test {
+	interface A {
+		default void m(int i) {  }
+	}
+	interface B extends A {
+
+	}
+	
+	class C {
+		public void m(int i) { }
+	}
+	
+	class D extends C implements B {
+
+	}
+}

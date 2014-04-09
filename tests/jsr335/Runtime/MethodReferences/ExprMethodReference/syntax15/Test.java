647d31ce6cf9ec1f88b280825574d0a472d53770
Erik <Erik.Hogeman@gmail.com>
2014-04-09 10:17:19 +0200

    Changed incomplete test case

--- tests/jsr335/Runtime/MethodReferences/ExprMethodReference/syntax15/Test.java
+++ tests/jsr335/Runtime/MethodReferences/ExprMethodReference/syntax15/Test.java
@@ -7,25 +7,28 @@ public class Test {
 		B m(int i, String s);
 	}
 	public interface B {
-		int m(String s);
+		C m(String s);
 	}
-	
-	public int getVal(int i) {
-		return i + 4;
+	public interface C {
+		int m();
 	}
 	
 	public static void main(String[] arg) {
 		int local = 5;
 		A a = (i, s1) -> {
-			if(local > 4)
+			if(local > i)
 				return s2 -> {
-					s2.length() + s1.length() + i;
+					return s2::length;
 				};
 			else
-				return s2 -> {
-					s1.length() + local;
-				};
+				return s2 -> s1::length;
 		};
-		testTrue(a.m("yes!") == 4);
+		
+		B b1 = a.m(4, "yes");
+		B b2 = a.m(6, "methodRef");
+		C c1 = b1.m("testing");
+		C c2 = b2.m("testing");
+		testTrue("MethodRef", c1.m() == 7);
+		testTrue("MethodRef", c2.m() == 9);
 	}
 }

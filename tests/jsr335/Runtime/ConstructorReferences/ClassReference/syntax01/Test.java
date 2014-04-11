bd66ac280b35f03784eda1f9d384077b591d87f8
Erik <Erik.Hogeman@gmail.com>
2014-04-11 11:59:30 +0200

    Added more constructor reference tests

---
+++ tests/jsr335/Runtime/ConstructorReferences/ClassReference/syntax01/Test.java
@@ -0,0 +1,16 @@
+import static runtime.Test.*;
+import java.util.*;
+
+public class Test {
+	public interface A {
+		Integer m(int i);
+	}
+	
+	public static void main(String[] arg) {
+		A a = Integer::new;
+		Integer i = a.m(20);
+		testTrue("ConstructorReference", i.intValue() == 20);
+		testTrue("ConstructorReference", i instanceof Integer);
+		testTrue("ConstructorReference", i.equals(new Integer(20)));
+	}
+}

5dc7b210b4bd11fc1b4244c324fa57a5145d9f41
Jesper Öqvist <jesper.oqvist@cs.lth.se>
2014-04-24 14:17:45 +0200

    Reorganized test files

---
+++ tests/jsr334/strings-in-switch/compile_01p/Test.java
@@ -0,0 +1,15 @@
+class Test {
+
+	void foo(String i) {
+		switch (i) {
+			case "foo":
+				break;
+			case "foo"+"bar":
+				break;
+			case "foo"+12:
+				break;
+			case ""+13:
+				break;
+		}
+	}
+}

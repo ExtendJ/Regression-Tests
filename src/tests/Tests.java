5ff813453a3850f2afa1d709fc66d33687a037a6
Jesper Öqvist <jesper.oqvist@cs.lth.se>
2015-02-16 16:13:58 +0100

    Refactored some tests

--- src/tests/Tests.java
+++ src/tests/Tests.java
@@ -9,7 +9,7 @@ public interface Tests {
 	 * Tests expected to fail.
 	 */
 	String[] FAILING = {
-		"generics/constructor01",
+		"generics/constructor_01f",
 		"type/conditional_expr02",
 		"type/ambiguous_01f",
 		"type/ambiguous_03f",
--- tests/generics/ParMethodDecl/A.java
+++
@@ -1,8 +0,0 @@
-public class A<P> {
-    {
-        m(null, null);
-    }
-
-    public <T> void m(T t, P p) {
-    }
-}

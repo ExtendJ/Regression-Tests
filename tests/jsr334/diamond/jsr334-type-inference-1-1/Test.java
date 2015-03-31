907b0449df8e04ca465c328949b82ea6d02f05aa
Jesper Öqvist <jesper.oqvist@cs.lth.se>
2015-03-31 17:24:00 +0200

    Refactored diamond test

--- tests/jsr334/diamond/jsr334-type-inference-1-1/Test.java
+++ tests/jsr334/diamond/jsr334-type-inference-1-1/Test.java
@@ -1,5 +1,7 @@
-public class T1_1 {
-    T1_1 (){}
+// Basic type inference
+// .result=COMPILE_PASS
+public class Test {
+    Test (){}
     public static void main(String[] args) {
 		java.util.ArrayList<Integer> foo = new java.util.ArrayList<>();
     }

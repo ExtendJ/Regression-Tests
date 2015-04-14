6bf8dcf81fcfac1e0f04ea8ffd43c5c3a73425fd
Jesper Öqvist <jesper.oqvist@cs.lth.se>
2015-04-14 17:16:50 +0200

    Added link to issue

--- tests/generics/cast_01f/Test.java
+++ tests/generics/cast_01f/Test.java
@@ -1,4 +1,5 @@
 // The type Object is not compatible with T extends Object
+// See https://bitbucket.org/jastadd/jastaddj/issue/105/object-is-not-compatible-with-type
 // .result=COMPILE_FAIL
 class Test<T> {
     T _() {

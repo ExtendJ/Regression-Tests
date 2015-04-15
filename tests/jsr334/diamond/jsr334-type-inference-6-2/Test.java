82876279336e292d4a4fbe579d110cf9f51e09af
Jesper Öqvist <jesper.oqvist@cs.lth.se>
2015-04-15 14:12:29 +0200

    Updated Java 7 diamond test

---
+++ tests/jsr334/diamond/jsr334-type-inference-6-2/Test.java
@@ -0,0 +1,17 @@
+// type argument inference through actual constructor arguments
+// .result=COMPILE_PASS
+class Test {
+    
+		class T6_2_A<T> {
+			public T a;
+			<U extends Throwable> T6_2_A(T t, U u) throws U {
+				a = t;
+			}
+		}
+		{
+			// in this invocation of T6_2_A, U is Error which is an unchecked exception type
+			// so we do not need to catch the exception which the constructor declares thrown
+			new T6_2_A<>("bar", new Error()).a.getBytes();
+		}
+	
+}

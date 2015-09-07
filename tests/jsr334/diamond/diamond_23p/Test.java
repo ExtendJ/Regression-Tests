1cf51d7a2bf714109f4e3a9e75d86b0dbc108e45
Jesper Öqvist <jesper.oqvist@cs.lth.se>
2015-09-07 14:36:39 +0200

    Added type inference tests

--- tests/jsr334/diamond/diamond_23p/Test.java
+++ tests/jsr334/diamond/diamond_23p/Test.java
@@ -2,15 +2,15 @@
 // .result=COMPILE_PASS
 class Test {
 
-  class T6_2_A<T> {
-    <U extends Throwable> T6_2_A(T t, U u) throws U {
-      throw u;
+  class T6_2_A<T extends Throwable> {
+    T6_2_A(T t) throws T {
+      throw t;
     }
   }
 
   {
     // In this invocation of T6_2_A, U is Error which is an unchecked exception type
     // so we do not need to catch the exception which the constructor declares thrown.
-    T6_2_A<String> _ = new T6_2_A<>("bar", new Error());
+    new T6_2_A<>(new Error());
   }
 }

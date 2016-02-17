b83433250723bb7eb71191eded5ec7e5addcd8d6
Jesper Öqvist <jesper.oqvist@cs.lth.se>
2016-02-17 19:37:41 +0100

    Update Java 8 excluded tests
    Exclude test that now work as acceptance tests for fixing long-term bugs.
    It is not helpful to see these test failures for each test run, better to just
    exclude them.

--- src/tests/extendj/TestJava8.java
+++ src/tests/extendj/TestJava8.java
@@ -16,42 +16,57 @@ import core.Util;
 @RunWith(Parameterized.class)
 public class TestJava8 {
 
-	private static final TestProperties properties = new TestProperties();
-	static {
-		properties.setProperty("compiler", "extendj");
-		properties.setProperty("extendj.jar", "extendj.jar"); // Default to local compiler Jar.
-		properties.exclude(tests.Tests.FAILING);
-		properties.exclude(tests.Tests.EXCLUDE_JAVA8);
+  private static final TestProperties properties = new TestProperties();
+  static {
+    properties.setProperty("compiler", "extendj");
+    properties.setProperty("extendj.jar", "extendj.jar"); // Default to local compiler Jar.
+    properties.exclude(tests.Tests.FAILING);
+    properties.exclude(tests.Tests.EXCLUDE_JAVA8);
 
-    // TODO(joqvist): fix these test errors in Java8:
-		properties.exclude("enum/final_07f");
-		properties.exclude("enum/final_09f");
-		properties.exclude("generics/override_02f");
-		properties.exclude("generics/override_10f");
-		properties.exclude("generics/override_12f");
-	}
+    // TODO(joqvist): These are bugs in ExtendJ that need to be fixed, but it is not helpful
+    // to see the test failures for each run. That could potentially hide new regressions.
+    properties.exclude("enum/final_07f");
+    properties.exclude("enum/final_09f");
+    properties.exclude("generics/override_02f");
+    properties.exclude("generics/override_10f");
+    properties.exclude("generics/override_12f");
+    properties.exclude("jsr335/Semantics/ConstructorReferenceAnalysis/ClassReferences/ShouldFail/syntax03");
+    properties.exclude("jsr335/Semantics/ConstructorReferenceAnalysis/ClassReferences/ShouldFail/syntax04");
+    properties.exclude("jsr335/Semantics/FunctionalInterfaces/ReturnTypeSubstitutable/ShouldCompile/syntax22");
+    properties.exclude("jsr335/Semantics/FunctionalInterfaces/ReturnTypeSubstitutable/ShouldFail/syntax11");
+    properties.exclude("jsr335/Semantics/FunctionalInterfaces/ReturnTypeSubstitutable/ShouldFail/syntax12");
+    properties.exclude("jsr335/Semantics/FunctionalInterfaces/Signature/ShouldCompile/syntax08");
+    properties.exclude("jsr335/Semantics/FunctionalInterfaces/Signature/ShouldCompile/syntax12");
+    properties.exclude("jsr335/Semantics/FunctionalInterfaces/Signature/ShouldFail/syntax08");
+    properties.exclude("jsr335/Semantics/FunctionalInterfaces/TypeParameters/ShouldFail/syntax02");
+    properties.exclude("jsr335/Semantics/FunctionalInterfaces/TypeParameters/ShouldFail/syntax03");
+    properties.exclude("jsr335/Semantics/LambdaTypeAnalysis/AssignmentContext/ShouldCompile/syntax28");
+    properties.exclude("jsr335/Semantics/MethodReferenceAnalysis/TypeMethodReference/ShouldCompile/syntax10");
+    properties.exclude("jsr335/Semantics/MethodReferenceAnalysis/TypeMethodReference/ShouldCompile/syntax11");
+    properties.exclude("jsr335/Semantics/MethodReferenceAnalysis/TypeMethodReference/ShouldCompile/syntax12");
+  }
 
-	private final String testDir;
+  private final String testDir;
 
-	/**
-	 * Construct a new JastAdd test
-	 * @param testDir The base directory for the test
-	 */
-	public TestJava8(String testDir) {
-		this.testDir = testDir;
-	}
+  /**
+   * Construct a new JastAdd test
+   * @param testDir The base directory for the test
+   */
+  public TestJava8(String testDir) {
+    this.testDir = testDir;
+  }
 
-	/**
-	 * Run the JastAdd test
-	 */
-	@Test
-	public void runTest() {
-		TestRunner.runTest(testDir, properties);
-	}
+  /**
+   * Run the JastAdd test
+   */
+  @Test
+  public void runTest() {
+    TestRunner.runTest(testDir, properties);
+  }
 
-	@SuppressWarnings("javadoc")
-	@Parameters(name = "{0}")
-	public static Iterable<Object[]> getTests() {
-		return Util.getTests(properties);
-	}
+  @SuppressWarnings("javadoc")
+  @Parameters(name = "{0}")
+  public static Iterable<Object[]> getTests() {
+    return Util.getTests(properties);
+  }
 }

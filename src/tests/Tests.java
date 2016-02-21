c92e63fe75ce695457004cd3a0c01fc0b7fcc8d6
Jesper Öqvist <jesper@llbit.se>
2016-02-21 23:36:56 +0100

    Add bridge method tests

--- src/tests/Tests.java
+++ src/tests/Tests.java
@@ -5,33 +5,34 @@ package tests;
  * @author Jesper Öqvist <jesper.oqvist@cs.lth.se>
  */
 public interface Tests {
-	/**
-	 * Tests expected to fail.
-	 */
-	String[] FAILING = {
-		"generics/constructor_01f",
-		"type/conditional_expr02",
-		"type/ambiguous_01f",
-	};
+  /**
+   * Tests expected to fail.
+   */
+  String[] FAILING = {
+    "generics/constructor_01f",
+    "type/conditional_expr02",
+    "type/ambiguous_01f",
+    "generics/bridge_method_04p",
+  };
 
-	/**
- 	 * Java 7 tests
- 	 */
-	String[] JAVA7 = {
-		"jsr334"
-	};
+  /**
+   * Java 7 tests.
+   */
+  String[] JAVA7 = {
+    "jsr334"
+  };
 
-	/**
- 	 * Java 8 tests
- 	 */
-	String[] JAVA8 = {
-		"jsr335"
-	};
+  /**
+   * Java 8 tests.
+   */
+  String[] JAVA8 = {
+    "jsr335"
+  };
 
-	/**
- 	 * Tests that should be excluded for Java 8
- 	 */
-	String[] EXCLUDE_JAVA8 = {
-		"jsr334/diamond/diamond_18"
-	};
+  /**
+   * Tests that should be excluded for Java 8.
+   */
+  String[] EXCLUDE_JAVA8 = {
+    "jsr334/diamond/diamond_18"
+  };
 }

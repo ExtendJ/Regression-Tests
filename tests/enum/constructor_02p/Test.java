f523309ce30a259cf9fc7b795643e720efd20a40
Jesper Öqvist <jesper.oqvist@cs.lth.se>
2016-02-23 23:26:06 +0100

    Improved enum tests

--- tests/enum/constructor_02p/Test.java
+++ tests/enum/constructor_02p/Test.java
@@ -1,14 +1,29 @@
-// Test calling constructor within constructor
-// .result=COMPILE_PASS
+// Test calling constructor within constructor.
+// .result=EXEC_PASS
+public class Test {
+  public static void main(String[] args) {
+    if (E.A0.i != 0xFFFF) {
+      throw new Error("A0");
+    }
+    if (E.A1.i != 123) {
+      throw new Error("A1");
+    }
+    if (E.A2.i != 0xFFFF) {
+      throw new Error("A2");
+    }
+  }
+}
+
 enum E {
-	;
-	int i;
+  A0, A1(123), A2;
 
-	private E() {
-		this(0xFFFF);
-	}
+  public int i;
 
-	E(int i) {
-		this.i = i;
-	}
+  private E() {
+    this(0xFFFF);
+  }
+
+  E(int i) {
+    this.i = i;
+  }
 }

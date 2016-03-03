50682edbef0f13b4d5246a3477a55dfb1089f82e
Jesper Öqvist <jesper.oqvist@cs.lth.se>
2016-03-03 10:46:31 +0100

    Add switch tests

--- tests/annotation/array_02f/Test.java
+++ tests/annotation/array_02f/Test.java
@@ -1,10 +1,11 @@
+// Annotation member types can not be nested array types.
 // .result=COMPILE_FAIL
 
-@interface annot {
-	String[][] names();
-}	
+@interface Annot {
+  String[][] names(); // Invalid annotation member type.
+}
 
 public class Test {
-	public void f() {
-	}
+  public void f() {
+  }
 }

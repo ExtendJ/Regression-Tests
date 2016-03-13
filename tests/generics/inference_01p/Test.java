1fcb443d9643e22cdb0d8f28ee54fcd025248177
Jesper Öqvist <jesper@llbit.se>
2016-03-13 11:08:54 +0100

    Update test description

--- tests/generics/inference_01p/Test.java
+++ tests/generics/inference_01p/Test.java
@@ -1,4 +1,11 @@
 // Assignment context type inference.
+// This test checks that two kinds of type substitution can happen
+// simultaneously: first, there is type substitution to substitute the
+// type parameter T of the class C in the type of the first argument
+// of the test method, then there is type substitution to substitute
+// the method type parameter R after type inference infers the type
+// R=Integer.
+// See https://bitbucket.org/extendj/extendj/issues/153/problem-with-type-inference-from-expected
 // .result=COMPILE_PASS
 public class Test {
   void test(C<String> c, Impl i) {

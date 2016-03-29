caba175066be86ee7924d47bbb796f626f8d03ad
Jesper Öqvist <jesper.oqvist@cs.lth.se>
2016-03-29 11:09:40 +0200

    Update parameterized constructor test case

--- tests/generics/constructor_08p/Test.java
+++ tests/generics/constructor_08p/Test.java
@@ -10,7 +10,21 @@ public class Test<U extends Number, V> {
     s = t;
   }
 
+  public <S> Test(S s, U u, V v) {
+    this.u = u;
+    this.v = v;
+  }
+
   public static void main(String[] args) {
+    // Anonymous class instance using parameterized constructor:
+    new <Float> Test<Long, String>(123f, 0xFFL, "123") {
+      @Override
+      public String toString() {
+        return "123";
+      }
+    };
+
+    // Regular class instance expression using parameterized constructor:
     new <Number, Float> Test<Long, String>(123, 0xFFL, 0.222f, "123");
   }
 }

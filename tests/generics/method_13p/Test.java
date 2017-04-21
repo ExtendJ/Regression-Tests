9991a74c5a2ea74fac0b27e5322a1180e237bf9f
Jesper Öqvist <jesper.oqvist@cs.lth.se>
2017-04-21 21:33:29 +0200

    Add generic method inference tests

---
+++ tests/generics/method_13p/Test.java
@@ -0,0 +1,24 @@
+// Test that most applicable method selection works with bounded type variables.
+import static runtime.Test.*;
+
+class Container<T> { }
+
+public class Test {
+
+  <T extends String> Container<T> g() {
+    return new Container<T>();
+  }
+
+  String foo(Container<Object> c) {
+    return "fails";
+  }
+
+  String foo(Object c) {
+    return "works";
+  }
+
+  public static void main(String[] args) {
+    Test test = new Test();
+    testEquals("works", test.foo(test.g()));
+  }
+}

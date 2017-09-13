d665860f62b0d0c3149cb983a733560eb402ab8b
Jesper Öqvist <jesper.oqvist@cs.lth.se>
2017-09-13 10:06:05 +0200

    Add name resolution test

--- tests/name/name01/T1.java
+++ tests/name/name01/T1.java
@@ -1,3 +1,6 @@
+// Tests accessing a protected member of an enclosing class inherited from
+// common superclass declared in another package
+// http://svn.cs.lth.se/trac/jastadd-trac/ticket/52
 import p1.C;
 class T1 extends C {
 	class Inner extends C {

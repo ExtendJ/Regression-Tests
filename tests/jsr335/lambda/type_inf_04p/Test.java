8209bb700147f0ce473c65130579cd64c2e79042
Jesper Öqvist <jesper.oqvist@cs.lth.se>
2017-11-24 12:00:31 +0100

    Rewrite test jsr335/lambda/type_inf_04p

--- tests/jsr335/lambda/type_inf_04p/Test.java
+++ tests/jsr335/lambda/type_inf_04p/Test.java
@@ -1,20 +1,19 @@
 // Test stack overflow in ExtendJ during ParseName rewrite.
 // https://bitbucket.org/extendj/extendj/issues/203/stack-overflow-caused-by-parsename-rewrite
 // .result: COMPILE_PASS
-import java.util.function.Function;
-
 class Token {
-  public final String string;
-  public Token(String token) {
-    this.string = token;
-  }
+  public String string;
+}
+
+interface Func<T> {
+  String apply(T t);
 }
 
 public interface Test {
-  <T> String transform(T in, Function<T, String> fun);
+  <T> String transform(T in, Func<T> fun);
 
-  default void prepend(String string) {
-    transform(new Token(string), token -> "x" + token.string.toString());
+  default void prepend(Token t) {
+    transform(t, token -> token.string.toString());
   }
 }
 

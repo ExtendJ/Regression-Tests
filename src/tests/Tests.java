eb8bfe3c4dca7341c34e2e826d7c2e8cc9a36af1
Jesper Öqvist <jesper.oqvist@cs.lth.se>
2018-01-04 14:36:15 +0100

    Add tests for diamond

--- src/tests/Tests.java
+++ src/tests/Tests.java
@@ -60,6 +60,8 @@ public interface Tests {
     "pkg/static_import_03p", // https://bitbucket.org/extendj/extendj/issues/227/error-should-not-be-generated-for-unused
     "jsr335/lambda/type_inf_09p", // True Java 8 type inference is needed.
     "curious", // Curious type inference problems in Java 8. Don't even work with javac.
+    "jsr335/diamond/generics_01p", // https://bitbucket.org/extendj/extendj/issues/267/diamond-constructor-inference-fails-if
+    "jsr335/diamond/nested_01p", // https://bitbucket.org/extendj/extendj/issues/266/stack-overflow-caused-by-nested-diamond
   };
 
   /**
--- tests/jsr334/diamond/diamond_18f/Test.java
+++ tests/jsr334/diamond/diamond_18f/Test.java
@@ -5,6 +5,7 @@ class Test {
   class T8_A<T> { }
 
   void l(T8_A<Integer> t) { }
+
   {
     l(new T8_A<>());
   }

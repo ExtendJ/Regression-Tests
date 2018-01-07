677e9e52ff363945cf0383491b8a6894b840cf8f
Jesper Öqvist <jesper@llbit.se>
2018-01-07 14:06:42 +0100

    Make stmt/assign_01p repeatable on Windows

--- tests/stmt/assign_01p/Test.java
+++ tests/stmt/assign_01p/Test.java
@@ -1,5 +1,8 @@
+import java.io.PrintStream;
+import java.io.UnsupportedEncodingException;
+
 public class Test {
-  public static void main(String[] args) {
+  public static void main(String[] args) throws UnsupportedEncodingException {
     new Test().run();
   }
 
@@ -9,13 +12,15 @@ public class Test {
   byte f_b;
   long f_l;
 
-  void run() {
+  void run() throws UnsupportedEncodingException {
     f_i = 230;
     f_s = (short) f_i;
     f_c = (char) f_i;
     f_b = (byte) f_i;
     f_l = f_i;
 
+    // Need to set locale for testing repeatability on Windows.
+    System.setOut(new PrintStream(System.out, true, "UTF-8"));
     System.out.println(f_i);
     System.out.println(f_s);
     System.out.println(f_c);

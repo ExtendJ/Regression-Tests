c651468e9039df2b9563434a627f021d346c9405
Jesper Öqvist <jesper.oqvist@cs.lth.se>
2018-01-10 16:45:18 +0100

    Fixed incorrect expected test result

--- tests/pkg/static_import_05f/Test.java
+++ tests/pkg/static_import_05f/Test.java
@@ -1,6 +1,6 @@
 // This should fail to compile because the type B is not imported!
 // https://bitbucket.org/extendj/extendj/issues/287/static-import-declaration-imports-too-much
-// .result: COMPILE_PASS
+// .result: COMPILE_FAIL
 import static pkg.A.newB;
 
 public class Test {

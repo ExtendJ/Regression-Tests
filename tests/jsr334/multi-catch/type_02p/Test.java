035acede599a60c8d90f22dbe32e46020e0e4bff
Jesper Öqvist <jesper.oqvist@cs.lth.se>
2018-01-30 07:38:19 +0100

    Add test for issue 298

--- tests/jsr334/multi-catch/type_02p/Test.java
+++ tests/jsr334/multi-catch/type_02p/Test.java
@@ -1,21 +1,22 @@
-// The effective type of a union-type catch parameter is the lub of
-// the union types.
+// The effective type of a union-type catch parameter is the least upper bound
+// of the union types.
 // .result: COMPILE_PASS
-public class T6 {
-	class MyException extends Exception {
-		public int a;
-	}
-	class E1 extends MyException {}
-	class E2 extends MyException {}
-	public void foo() {
+public class Test {
+	public void pass() {
 		try {
 			if (System.currentTimeMillis() % 2 == 0)
 				throw new E1();
 			else
 				throw new E2();
 		} catch (E1 | E2 e) {
-			// e has effective type MyException
+			// Catch parameter e has effective type MyException.
 			e.a = 0xDeadBeef;
 		}
 	}
 }
+
+class MyException extends Exception {
+	public int a;
+}
+class E1 extends MyException {}
+class E2 extends MyException {}
